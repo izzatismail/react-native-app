2815a6edc94956f962cde1d1ea2dc33f9ac54a31
Izzat Ismail <mizzat1996@gmail.com>
2023-05-05 21:40:53 +0800

    learnt about text & image in RN

--- App.tsx
+++ App.tsx
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+  SafeAreaView,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+} from "react-native";
 
+console.log("Hello World!");
+
+// View -> UIView
 export default function App() {
+  const handlePress = () => {
+    console.log("Text clicked");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}></SafeAreaView>
+
+    // Text & Image code
+    // <SafeAreaView style={styles.container}>
+    //   <Text numberOfLines={1} onPress={handlePress}>
+    //     Hello React Native World!
+    //   </Text>
+    //   <TouchableNativeFeedback onPress={handlePress}>
+    //     <Image
+    //       fadeDuration={500}
+    //       source={{
+    //         width: 200,
+    //         height: 300,
+    //         uri: "https://picsum.photos/200/300",
+    //       }}
+    //     />
+    //   </TouchableNativeFeedback>
+    // </SafeAreaView>
   );
+  // Local image use require() function Example: require("./assets/favicon.png")
+  // For network images, apart from the URI, we need to explicitly set the width & height
+  // Example: source={{width: 200, height: 300, uri: "https://picsum.photos/200/300"}}
+  // fadeDuration ONLY WORKS ON ANDROID
+  // Touchable allows us to set anything in our view touchable (Image for example has no onPress function, so use touchable)
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, // Boolean, shows whether the view is flexible or not
+    backgroundColor: "#fff", // Can be hex, or value
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
