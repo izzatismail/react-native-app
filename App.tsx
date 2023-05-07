f7198b47e946d00bcf4c6cf272823ee2efcc00f6
Izzat Ismail <mizzat1996@gmail.com>
2023-05-07 13:02:01 +0800

    learnt about dimensions & device orientations

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useDeviceOrientation } from "@react-native-community/hooks";
 import {
   StyleSheet,
   Text,
@@ -10,76 +11,33 @@ import {
   TouchableNativeFeedback,
   Alert,
   Button,
-  Platform, // This is used to determine what platfrom it is (either iOS or Android)
+  Platform,
   StatusBar,
+  Dimensions,
 } from "react-native";
-
-console.log("Hello World!");
 
 // View -> UIView
 export default function App() {
-  const handlePress = () => {
-    console.log("Text clicked");
-  };
+  const orientation = useDeviceOrientation();
+  console.log(orientation);
 
   return (
-    // There can be multiple style, defined in array -> style={[styles.container], [styles.container]}
-    // Also, SafeAreaView is only for iOS
     <SafeAreaView style={styles.container}>
-      <Button
-        color="orange"
-        title="Click Me!"
-        onPress={
-          () =>
-            // Alert.alert example
-            Alert.alert("My Title", "My Message", [
-              {
-                text: "Yes",
-                onPress: () => console.log("Yes Clicked"),
-              },
-              {
-                text: "No",
-                onPress: () => console.log("No Clicked"),
-              },
-            ])
-
-          // Alert.prompt example ONLY WORK ON IOS
-          // Alert.prompt("My Title", "My Message", (text) => console.log(text))
-        }
-      />
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "100%", // Can be in percentage
+          height: orientation === "portrait" ? "100%" : "50%", // These numbers are called Density-Independent pixels
+        }}
+      ></View>
     </SafeAreaView>
-
-    // Text & Image code
-    // <SafeAreaView style={styles.container}>
-    //   <Text numberOfLines={1} onPress={handlePress}>
-    //     Hello React Native World!
-    //   </Text>
-    //   <TouchableNativeFeedback onPress={handlePress}>
-    //     <Image
-    //       fadeDuration={500}
-    //       source={{
-    //         width: 200,
-    //         height: 300,
-    //         uri: "https://picsum.photos/200/300",
-    //       }}
-    //     />
-    //   </TouchableNativeFeedback>
-    // </SafeAreaView>
   );
-  // Local image use require() function Example: require("./assets/favicon.png")
-  // For network images, apart from the URI, we need to explicitly set the width & height
-  // Example: source={{width: 200, height: 300, uri: "https://picsum.photos/200/300"}}
-  // fadeDuration ONLY WORKS ON ANDROID
-  // Touchable allows us to set anything in our view touchable (Image for example has no onPress function, so use touchable)
 }
 
-// This is actually just a JS object
-// 1- This method validates the property we used
-// 2- RN is working on optimization, recommended practice
 const styles = StyleSheet.create({
   container: {
-    flex: 1, // Boolean, shows whether the view is flexible or not
-    backgroundColor: "#fff", // Can be hex, or value
+    flex: 1,
+    backgroundColor: "#fff",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
